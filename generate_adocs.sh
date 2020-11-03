01abec615609742ac5b0b8dc6f7b77b0f8a7836e
hen <dev@flamefew.com>
2020-11-02 23:50:22 -0800

    Asciidoccing the Expanded Rules

--- generate_adocs.sh
+++ generate_adocs.sh
@@ -1,2 +1,4 @@
 asciidoctor docs/rules/Tournament_Rules.adoc 
 asciidoctor-pdf docs/rules/Tournament_Rules.adoc 
+asciidoctor docs/rules/Expanded_Rules_2.0.adoc 
+asciidoctor-pdf docs/rules/Expanded_Rules_2.0.adoc 
