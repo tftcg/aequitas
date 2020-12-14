d16e16e430ad17cc9bf31b31ec110ab0a79eae34
hen <dev@flamefew.com>
2020-12-13 17:51:14 -0800

    Updating 1 with bugfix; adding 2

--- generate_adocs.sh
+++ generate_adocs.sh
@@ -9,3 +9,6 @@ asciidoctor-pdf docs/rules/Expanded_Rules-DRAFT.adoc
 
 asciidoctor docs/rules/Expanded_Rules-RFC-1.adoc 
 asciidoctor-pdf docs/rules/Expanded_Rules-RFC-1.adoc 
+
+asciidoctor docs/rules/Expanded_Rules-RFC-2.adoc 
+asciidoctor-pdf docs/rules/Expanded_Rules-RFC-2.adoc 
