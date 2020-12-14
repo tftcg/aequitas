489c973b920c1572c62c4427ff33ad383a5b1a9c
hen <dev@flamefew.com>
2020-12-13 17:26:15 -0800

    Adding RFC-1: Upgrades

--- generate_adocs.sh
+++ generate_adocs.sh
@@ -1,6 +1,11 @@
 asciidoctor docs/rules/Tournament_Rules.adoc 
 asciidoctor-pdf docs/rules/Tournament_Rules.adoc 
+
 asciidoctor docs/rules/Expanded_Rules.adoc 
 asciidoctor-pdf docs/rules/Expanded_Rules.adoc 
+
 asciidoctor docs/rules/Expanded_Rules-DRAFT.adoc 
 asciidoctor-pdf docs/rules/Expanded_Rules-DRAFT.adoc 
+
+asciidoctor docs/rules/Expanded_Rules-RFC-1.adoc 
+asciidoctor-pdf docs/rules/Expanded_Rules-RFC-1.adoc 
