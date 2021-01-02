e07e04a1dfcc14d9b83d1f40735e848637f358f4
hen <dev@flamefew.com>
2021-01-02 13:47:01 -0800

    Renamed RFC-3

--- generate_adocs.sh
+++ generate_adocs.sh
@@ -7,11 +7,11 @@ asciidoctor-pdf docs/rules/Expanded_Rules.adoc
 asciidoctor docs/rules/Expanded_Rules-DraftErrata.adoc 
 asciidoctor-pdf docs/rules/Expanded_Rules-DraftErrata.adoc 
 
-asciidoctor docs/rules/Expanded_Rules-DRAFT.adoc 
-asciidoctor-pdf docs/rules/Expanded_Rules-DRAFT.adoc 
-
 asciidoctor docs/rules/Expanded_Rules-RFC-1.adoc 
 asciidoctor-pdf docs/rules/Expanded_Rules-RFC-1.adoc 
 
 asciidoctor docs/rules/Expanded_Rules-RFC-2.adoc 
 asciidoctor-pdf docs/rules/Expanded_Rules-RFC-2.adoc 
+
+asciidoctor docs/rules/Expanded_Rules-RFC-3.adoc 
+asciidoctor-pdf docs/rules/Expanded_Rules-RFC-3.adoc 
