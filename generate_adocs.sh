26fddbf8f251719b6537ea2040d97e74ce9d5d49
hen <dev@flamefew.com>
2021-01-03 15:46:27 -0800

    DraftErrata deleted

--- generate_adocs.sh
+++ generate_adocs.sh
@@ -4,9 +4,6 @@ asciidoctor-pdf docs/rules/Tournament_Rules.adoc
 asciidoctor docs/rules/Expanded_Rules.adoc 
 asciidoctor-pdf docs/rules/Expanded_Rules.adoc 
 
-asciidoctor docs/rules/Expanded_Rules-DraftErrata.adoc 
-asciidoctor-pdf docs/rules/Expanded_Rules-DraftErrata.adoc 
-
 asciidoctor docs/rules/Expanded_Rules-RFC-1.adoc 
 asciidoctor-pdf docs/rules/Expanded_Rules-RFC-1.adoc 
 
