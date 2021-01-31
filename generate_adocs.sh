232feed5b225db83b430c4426a219ac6e79860a4
hen <dev@flamefew.com>
2021-01-31 15:32:36 -0800

    Creating draft with RFC-1 and RFC-3 included

--- generate_adocs.sh
+++ generate_adocs.sh
@@ -4,11 +4,14 @@ asciidoctor-pdf docs/rules/Tournament_Rules.adoc
 asciidoctor docs/rules/Expanded_Rules.adoc 
 asciidoctor-pdf docs/rules/Expanded_Rules.adoc 
 
-asciidoctor docs/rules/Expanded_Rules-RFC-1.adoc 
-asciidoctor-pdf docs/rules/Expanded_Rules-RFC-1.adoc 
+asciidoctor docs/rules/Expanded_Rules-draft.adoc 
+asciidoctor-pdf docs/rules/Expanded_Rules-draft.adoc 
+
+#asciidoctor docs/rules/Expanded_Rules-RFC-1.adoc 
+#asciidoctor-pdf docs/rules/Expanded_Rules-RFC-1.adoc 
 
 asciidoctor docs/rules/Expanded_Rules-RFC-2.adoc 
 asciidoctor-pdf docs/rules/Expanded_Rules-RFC-2.adoc 
 
-asciidoctor docs/rules/Expanded_Rules-RFC-3.adoc 
-asciidoctor-pdf docs/rules/Expanded_Rules-RFC-3.adoc 
+#asciidoctor docs/rules/Expanded_Rules-RFC-3.adoc 
+#asciidoctor-pdf docs/rules/Expanded_Rules-RFC-3.adoc 
